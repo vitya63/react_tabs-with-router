035f1ebdda296f38f967b9f9528861c604abb555
Viktor Lukovets <nuerio63@gmail.com>
2019-11-06 22:52:16 +0200

    Updates

---
+++ precache-manifest.1d3aac2bf8454964da594c25aea4f339.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546b276ea62c48a3615ac2d2c305600d",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "e1824ea559fc57883964",
+    "url": "/react_tabs-with-router/static/css/main.0a140088.chunk.css"
+  },
+  {
+    "revision": "8029f14a718d0dc79366",
+    "url": "/react_tabs-with-router/static/js/2.92cabb85.chunk.js"
+  },
+  {
+    "revision": "e1824ea559fc57883964",
+    "url": "/react_tabs-with-router/static/js/main.1d447d1c.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
